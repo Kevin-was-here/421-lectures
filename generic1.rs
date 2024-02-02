94aef5b49972dfb27d1faba503ea6ddc70f1a63b
kevin-was-here <code --wait>
2024-02-02 13:08:39 -0700

    setup

---
+++ generic1.rs
@@ -0,0 +1,12 @@
+fn print_max<T>(x: T, y: T) {
+    if x >= y {
+        println!("{x} >= {y}");
+    } else {
+        println!("{y} > {x}");
+    }
+}
+
+fn main() {
+    print_max::<u16>(123, 321);
+    print_max(3.14, 1.414);
+}
